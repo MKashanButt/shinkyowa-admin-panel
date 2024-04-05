05436a85fb842585e40aebb8f8b11135957a8a8e
MKashanButt <kashanbutt566@gmail.com>
2024-04-06 01:10:25 +0500

    updated section

--- src/Pages/Accounts/Sub-Pages/CustomerAccount/components/Table.jsx
+++ src/Pages/Accounts/Sub-Pages/CustomerAccount/components/Table.jsx
@@ -3,82 +3,33 @@ import styles from '../CustomerAccount.module.css'
 
 const Table = () => {
     return (
-        <table className={styles.account}>
-            <thead>
-                <tr>
-                    <th>S.No</th>
-                    <th>Date</th>
-                    <th>Vehicle Name</th>
-                    <th>Chasis</th>
-                    <th>Total C&F</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-                <tr>
-                    <td>01.</td>
-                    <td>04/04/2024</td>
-                    <td>Toyota Corolla 2016</td>
-                    <td>CRF1456</td>
-                    <td>$6000</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className={styles.table}>
+            <table className={styles.account}>
+                <thead>
+                    <tr>
+                        <th>S.No</th>
+                        <th>Date</th>
+                        <th>Vehicle Name</th>
+                        <th>Chasis</th>
+                        <th>Total C&F</th>
+                        <th>Payment Recieved</th>
+                        <th>Balance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>01.</td>
+                        <td>04/04/2024</td>
+                        <td>Toyota Corolla 2016</td>
+                        <td>CRF1456</td>
+                        <td>$6000</td>
+                        <td>08/04/2024</td>
+                        <td>$4000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
     )
 }
 
