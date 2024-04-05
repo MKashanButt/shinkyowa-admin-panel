f50570ee8d8100130d1059fbeafa3c834dcf9f2f
MKashanButt <kashanbutt566@gmail.com>
2024-04-06 00:46:07 +0500

    updated styling

--- src/Pages/Accounts/Sub-Pages/CustomerAccount/Index.jsx
+++ src/Pages/Accounts/Sub-Pages/CustomerAccount/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Table from './components/Table';
 import Breadcrumbs from '../../../../components/Breadcrumbs/Index';
+import TableHeading from '../../../../components/TableHeading/Index';
 
 import styles from './CustomerAccount.module.css';
 
@@ -9,11 +10,8 @@ const CompanyAccount = () => {
         <>
             <section className={styles.customerAccount}>
                 <Breadcrumbs mainPage="Account" subPage="Customer Account" />
+                <TableHeading heading="Customer account" pageUrl="add-customer-data" />
                 <Table />
-                <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                </svg>
-                    Add Record</button>
             </section>
         </>
     )
--- src/Pages/Accounts/Sub-Pages/Payments/Index.jsx
+++ src/Pages/Accounts/Sub-Pages/Payments/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Breadcrumbs from '../../../../components/Breadcrumbs/Index';
 import Table from './components/Table';
+import TableHeading from '../../../../components/TableHeading/Index';
 
 import styles from './Payments.module.css';
 
@@ -9,11 +10,8 @@ const Payments = () => {
         <>
             <section className={styles.payments}>
                 <Breadcrumbs mainPage="Account" subPage="Company Account" />
+                <TableHeading heading="Payments" pageUrl="add-payment-data" />
                 <Table />
-                <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                </svg>
-                    Add Record</button>
             </section>
         </>
     )
